c2635fa9817202b604c8414880247bbd0b9df78a
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2025-04-08 00:36:13 +0530

    css changes

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-neutral-500 mt-auto">
-      <div className="container mx-auto p-4">
-        <p className="text-center text-white">
-          &copy; {new Date().getFullYear()} WiFi Signal Simulator | Built with Next.js
+    <footer className="bg-neutral-100 mt-auto">
+      <div className="container mx-auto p-4 flex gap-2 justify-center items-center">
+        <p className="text-center">
+          &copy; {new Date().getFullYear()} WiFi Planner | Built with 
         </p>
+        <Image src="/next.svg" alt="Next.js Logo" width={64} height={64} color='#fff'/>
       </div>
     </footer>
   );
