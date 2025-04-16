99b1db7c7ee721dda47758741ce89a658f8adca7
Soham Dutta <77527904+shm-dtt@users.noreply.github.com>
2025-04-16 22:25:03 +0530

    UI changes (#4)
    * Minor changes of colors, logo, legends
    * Icon changed, minor header/footer changes

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
+
 const Header: React.FC = () => {
     return (
-      <header className="bg-neutral-100 w-full">
-        <div className="p-4 text-center">
-          <h1 className="text-2xl font-medium">Nest*</h1>
+      <header className=" border-b-2 w-full">
+        <div className="p-4 text-center mb-2">
+          <div className="text-2xl font-medium flex flex-row w-full justify-center items-center gap-2">
+            <Image src="/favicon.ico" alt="Nest Logo" width={24} height={24} /> 
+            Nest*
+            </div>
           <p className="text-gray-500 text-sm">
             Design/Upload your floor plan and find the best spot for your Wi-Fi router.
           </p>
